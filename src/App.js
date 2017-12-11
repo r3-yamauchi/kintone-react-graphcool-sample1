36a80b4f2aa33887ef769bf8810552e8f332805f
Hayato Yamauchi <yamauchi@r3it.com>
2017-12-11 13:03:30 +0900

    Subscription サポート

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+// import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 import ArticleList from './ArticleList';
 
-const link = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/xxxxxxxx99x9x99999xx99xxx',
+const serviceId = "xxxxxxxx99x9x99999xx99xxx";
+const httpLink = createHttpLink({ uri: `https://api.graph.cool/simple/v1/${serviceId}` });
+
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/${serviceId}`,
+  options: {
+    reconnect: true
+  }
 });
-const cache = new InMemoryCache();
-const client = new ApolloClient({ link, cache });
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+});
 
 const App = () => (
   <ApolloProvider client={client}>
--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,20 +1,11 @@
-import React from "react";
-import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import React, { Component } from 'react';
+import { withApollo, graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
 
 const ArticleItem = ({ article }) => (
   <li style={{ width: 600 }}>
     <h3>{article.title}</h3>
   </li>
-);
-
-const ArticleList = ({ data: { loading, error, allArticles } }) => (
-  loading ? <p>ロードしています...</p> :
-    error ? <p>Error: {error}</p> : (
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        {allArticles.map(article => <ArticleItem key={article.id} article={article} />)}
-      </ul>
-    )
 );
 
 const query = gql`
@@ -26,4 +17,71 @@ const query = gql`
 }
 `;
 
-export default graphql(query)(ArticleList);
+class ArticleList extends Component {
+
+  state = {
+    allArticles: []
+  }
+
+  async componentDidMount() {
+    this.Subscription = this.props.allQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Article(filter: {
+            mutation_in: [CREATED, UPDATED]
+          }) {
+            mutation
+            node {
+              id
+              title
+            }
+          }
+        }
+      `,
+      variables: null,
+      updateQuery: (previousState, { subscriptionData }) => {
+        console.dir(subscriptionData);
+        const data = subscriptionData.data.Article;
+        if (data.mutation === 'CREATED') {
+          const newArticle = data.node;
+          const articles = previousState.allArticles.concat([newArticle]);
+          return {
+            allArticles: articles
+          };
+        } else if (data.mutation === 'UPDATED') {
+          const articles = previousState.allArticles.slice();
+          const updatedArticle = data.node;
+          const oldArticleIndex = articles.findIndex(x => {
+            return updatedArticle.id === x.id;
+          });
+          articles[oldArticleIndex] = updatedArticle;
+          return {
+            allArticles: articles
+          };
+        }
+        return previousState;
+      }
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.allQuery.allArticles) {
+      this.setState({
+        allArticles: nextProps.allQuery.allArticles
+      });
+    }
+  }
+
+  render() {
+    return (
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {Boolean(this.state.allArticles) && this.state.allArticles.map(article => <ArticleItem key={article.id} article={article} />)}
+      </ul>
+    );
+  }
+
+}
+
+export default compose(
+  graphql(query, { name: 'allQuery' })
+)(withApollo(ArticleList));
